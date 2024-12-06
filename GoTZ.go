6f64fac7e6aca4455e9f790c5dabd0687f69e910
Jumican <86297574+Jumican@users.noreply.github.com>
2024-12-06 07:46:41 +0300

    Add files via upload

--- GoTZ.go
+++ GoTZ.go
@@ -13,6 +13,7 @@ func main() {
 	var isRome bool
 
 	_, err := fmt.Scanln(&x1, &op, &x2)
+	
 	if err != nil {
 		fmt.Println("Panic!")
 		os.Exit(0)
@@ -26,17 +27,17 @@ func main() {
 		n2, _ = strconv.Atoi(x2)
 		isRome = false
 	} else {
-			roman := map[string]int{
-				"I": 1,
-				"II": 2,
-				"III": 3,
-				"IV": 4,
-				"V": 5,
-				"VI": 6,
-				"VII": 7,
+			roman := map[string]int {
+				"I":    1,
+				"II":   2,
+				"III":  3,
+				"IV":   4,
+				"V":    5,
+				"VI":   6,
+				"VII":  7,
 				"VIII": 8,
-				"IX": 9,
-				"X": 10,
+				"IX":   9,
+				"X":    10,
 			}
 			n1 = roman[x1]
 			n2 = roman[x2]
@@ -53,88 +54,60 @@ func main() {
 	}
 
 	if op == "+" {
-		sum = n1 + n2
+			sum = n1 + n2
+	} else if op == "-" {
+			sum = n1 - n2
+	} else if op == "*" {
+			sum = n1 * n2
+	} else if op == "/" {
+			sum = n1 / n2
 	} else {
-			if op == "-" {
-				sum = n1 - n2
-			} else {
-					if op == "*" {
-						sum = n1 * n2
-					} else {
-							if op == "/" {
-								sum = n1 / n2
-							} else {
-									fmt.Println("Panic!")
-									os.Exit(0)
-								}
-						}
-				}
-		}
+			fmt.Println("Panic!")
+			os.Exit(0)
+	}
 
 	if isRome == false {
-		fmt.Println(sum)
-	}	else {
-			if sum <= 0 {
-				fmt.Println("Panic!")
-				os.Exit(0)
-			} else {
-					arabic := map[int]string{
-						1:"I",
-						2:"II",
-						3:"III",
-						4:"IV",
-						5:"V",
-						6:"VI",
-						7:"VII",
-						8:"VIII",
-						9:"IX",
-					}
+			fmt.Println(sum)
+	}	else if sum <= 0 {
+			fmt.Println("Panic!")
+			os.Exit(0)
+	} else {
+			arabic := map[int]string{
+				1:"I",
+				2:"II",
+				3:"III",
+				4:"IV",
+				5:"V",
+				6:"VI",
+				7:"VII",
+				8:"VIII",
+				9:"IX",
+			}
 
-					d := arabic[sum%10]
+			d := arabic[sum%10]
 
-					if sum < 10 {
-						fmt.Println(d)
-					} else {
-							if sum < 20 {
-								fmt.Println("X"+d)
-							} else {
-									if sum < 30 {
-										fmt.Println("XX"+d)
-									} else {
-											if sum < 40 {
-												fmt.Println("XXX"+d)
-											} else {
-													if sum < 50 {
-														fmt.Println("XL"+d)
-													} else {
-															if sum < 60 {
-																fmt.Println("L"+d)
-															} else {
-																	if sum < 70 {
-																		fmt.Println("LX"+d)
-																	} else {
-																			if sum < 80 {
-																				fmt.Println("LXX"+d)
-																			} else {
-																					if sum < 90 {
-																						fmt.Println("LXXX"+d)
-																					}else {
-																							if sum < 100 {
-																								fmt.Println("XC"+d)
-																							} else {
-																									if sum == 100 {
-																										fmt.Println("C")
-																									}
-																								}
-																						}
-																				}
-																		}
-																}
-														}
-												}
-										}
-								} 
-						}
-				}
-		}
+			if sum < 10 {
+					fmt.Println(d)
+			} else if sum < 20 {
+					fmt.Println("X"+d)
+			} else if sum < 30 {
+					fmt.Println("XX"+d)
+			} else if sum < 40 {
+					fmt.Println("XXX"+d)
+			} else if sum < 50 {
+					fmt.Println("XL"+d)
+			} else if sum < 60 {
+					fmt.Println("L"+d)
+			} else if sum < 70 {
+					fmt.Println("LX"+d)
+			} else if sum < 80 {
+					fmt.Println("LXX"+d)
+			} else if sum < 90 {
+					fmt.Println("LXXX"+d)
+			} else if sum < 100 {
+					fmt.Println("XC"+d)
+			} else if sum == 100 {
+					fmt.Println("C")
+			}
+	}
 }
